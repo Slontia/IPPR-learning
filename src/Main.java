5ae222dffa64d73e0359d8766a190b17ca119a55
slontia <654867229@qq.com>
2018-03-17 00:42:22 +0800

    finish report

--- src/Main.java
+++ src/Main.java
@@ -264,18 +264,19 @@ class HistogramCorrector extends ImageProcessor {
 
 public class Main {
 	public static void main(String[] args) {
-		String filename = "D:/input——b.jpg";
-		HistogramCorrector histogramCorrector = new HistogramCorrector(filename);
-		histogramCorrector.grayscale("origin");
-		histogramCorrector.histogramCorrection("hist_cor_1", 1); 		// task 1
-		histogramCorrector.histogramCorrection("hist_cor_2", 2); 		// task 1
-		histogramCorrector.histogramCorrection("hist_cor_3", 3); 		// task 1
-		histogramCorrector.histogramCorrection("hist_cor_4", 4); 		// task 1
-		histogramCorrector.histogramCorrection("hist_cor_5", 5); 		// task 1
+		String filename = "D:/input_3.jpg";
+//		HistogramCorrector histogramCorrector = new HistogramCorrector(filename);
+//		histogramCorrector.grayscale("origin");
+//		histogramCorrector.histogramCorrection("hist_cor_1", 1); 		// task 1
+//		histogramCorrector.histogramCorrection("hist_cor_2", 2); 		// task 1
+//		histogramCorrector.histogramCorrection("hist_cor_3", 3); 		// task 1
+//		histogramCorrector.histogramCorrection("hist_cor_4", 4); 		// task 1
+//		histogramCorrector.histogramCorrection("hist_cor_5", 5); 		// task 1
 		
 		LinearStretcher linearStretcher = new LinearStretcher(filename);
-		linearStretcher.globalStretch("str_global", 256/8*1, 256/8*7);
-		linearStretcher.localStretch("str_local", 256/8*3, 256/8*5, 256/8*2, 256/8*6);	// task 2
-		linearStretcher.piecewiseStretch("str_piece", 256/8*3, 256/8*5, 256/8*2, 256/8*6);	// task 2
+//		linearStretcher.grayscale("cas_str_global");
+//		linearStretcher.globalStretch("str_global", 0, 256);
+		linearStretcher.localStretch("str_local", 0, 90, 0, 256);	// task 2
+//		linearStretcher.piecewiseStretch("str_piece", 256/8*3, 256/8*5, 256/8*2, 256/8*6);	// task 2
 	}
 }
